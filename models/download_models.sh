b4f65a52a128cfb3ade53e4833fe942dfbb43f53
babu <shiv.surya314@gmail.com>
2018-01-27 13:33:05 +0530

    Corrected paths for model download

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Downloading model..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kwDeK7qNzjxpCTAiCoKdPCCp7g-ntg9n' -O ./lstm_model_1lyr512hid_earlystopped.npz
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1kwDeK7qNzjxpCTAiCoKdPCCp7g-ntg9n' -O models/lstm_model_1lyr512hid_earlystopped.npz
 echo "Downloading predictions..."
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11fnnZJZ92qGn1ZX9V1kjtDAkz7U2IemQ' -O ./predictions_lstm_1lyr512hid.npz
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11fnnZJZ92qGn1ZX9V1kjtDAkz7U2IemQ' -O models/predictions_lstm_1lyr512hid.npz
